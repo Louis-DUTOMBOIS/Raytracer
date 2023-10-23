4ffaace6f17026a6547523ab4ca317599d238fc7
Alex Pochet <thypux@gmail.com>
2023-10-23 14:34:29 +0200

    Ajout classe lumière directionnelle avec doc

---
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/DirectionalLight.java
@@ -0,0 +1,26 @@
+package fr.univartois.butinfo.sae301;
+
+/**
+ * Represents a directional light source in a 3D scene.
+ */
+public class DirectionalLight extends Light {
+    /**
+     * Constructs a directional light with the specified direction and color.
+     *
+     * @param direction The direction in which the light is emitted.
+     * @param color     The color of the light source.
+     */
+    public DirectionalLight(Triplet direction, Triplet color) {
+        super(direction, color);
+    }
+
+    /**
+     * Get the type of the light source, which is Directional.
+     *
+     * @return The type of the light source.
+     */
+    @Override
+    public LightType getType() {
+        return LightType.DIRECTIONAL;
+    }
+}
