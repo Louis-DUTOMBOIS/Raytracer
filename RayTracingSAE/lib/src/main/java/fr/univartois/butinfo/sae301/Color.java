82fa14fa8cfc00c105e41407eb4f0c0f9d9e3c3f
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2023-10-23 15:11:12 +0200

    Ajout de la class Color

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Color.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Color.java
@@ -1,5 +1,27 @@
 package fr.univartois.butinfo.sae301;
 
 public class Color {
+	Triplet trip;
+    public Color(double x, double y, double z) {
+        this.trip = new Triplet(x,y,z);
+    }
+    
+    public Triplet getTrip() {
+		return trip;
+	}
 
+    public Color add(Color c) {
+    	Triplet result = trip.add(c.getTrip());
+        return new Color(result.getX(), result.getY() , result.getZ());
+    }
+
+    public Color multiplicationScailary(double d) {
+    	Triplet result = trip.multiplicationScailary(d);
+        return new Color(result.getX(), result.getY() , result.getZ());
+    }
+
+    public Color schurProduct(Color c) {
+    	Triplet result = trip.schurProduct(c.getTrip());
+        return new Color(result.getX(), result.getY() , result.getZ());
+    }
 }
