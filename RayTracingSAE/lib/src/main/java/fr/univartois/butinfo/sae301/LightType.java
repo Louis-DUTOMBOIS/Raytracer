d3afed1035a0edad0222e6d42be3a398840b8b7f
Alex Pochet <thypux@gmail.com>
2023-10-23 14:32:09 +0200

    Ajout classe enumération pour le type de Light avec doc

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/LightType.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/LightType.java
@@ -1,6 +1,7 @@
 package fr.univartois.butinfo.sae301;
 
 public enum LightType {
-    DIRECTIONAL, 
-    POINT       
+    DIRECTIONAL, // Represents a directional light source that emits light in a specific direction.
+    
+    POINT     // Represents a point light source that emits light from a specific point in space.
 }
