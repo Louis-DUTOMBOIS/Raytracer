ab8e1b8ed9510fc5fb336e5e743285a7fc64c0a1
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2023-10-24 11:16:55 +0200

    Ajout de la JavaDoc

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Color.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Color.java
@@ -1,28 +1,64 @@
 package fr.univartois.butinfo.sae301;
 
+/**
+ * Represents a color using three components (RGB) stored as a Triplet.
+ * This class provides methods for performing color operations such as addition,
+ * scalar multiplication, and Schur product.
+ */
 public class Color {
-	
-	private Triplet trip;
+
+    private Triplet trip;
+
+    /**
+     * Constructs a Color object with the specified RGB components.
+     *
+     * @param x The red component of the color.
+     * @param y The green component of the color.
+     * @param z The blue component of the color.
+     */
     public Color(double x, double y, double z) {
-        this.trip = new Triplet(x,y,z);
+        this.trip = new Triplet(x, y, z);
     }
-    
+
+    /**
+     * Gets the Triplet representing the RGB components of the color.
+     *
+     * @return The Triplet object containing the RGB components.
+     */
     public Triplet getTrip() {
-		return trip;
-	}
+        return trip;
+    }
 
+    /**
+     * Adds another color to this color.
+     *
+     * @param c The Color object to add to this color.
+     * @return A new Color object representing the result of the addition.
+     */
     public Color add(Color c) {
-    	Triplet result = trip.add(c.getTrip());
-        return new Color(result.getX(), result.getY() , result.getZ());
+        Triplet result = trip.add(c.getTrip());
+        return new Color(result.getX(), result.getY(), result.getZ());
     }
 
+    /**
+     * Multiplies the color by a scalar value.
+     *
+     * @param d The scalar value for multiplication.
+     * @return A new Color object representing the result of the scalar multiplication.
+     */
     public Color multiplicationScailary(double d) {
-    	Triplet result = trip.multiplicationScailary(d);
-        return new Color(result.getX(), result.getY() , result.getZ());
+        Triplet result = trip.multiplicationScailary(d);
+        return new Color(result.getX(), result.getY(), result.getZ());
     }
 
+    /**
+     * Performs a Schur product (component-wise multiplication) with another color.
+     *
+     * @param c The Color object to perform the Schur product with.
+     * @return A new Color object representing the result of the Schur product.
+     */
     public Color schurProduct(Color c) {
-    	Triplet result = trip.schurProduct(c.getTrip());
-        return new Color(result.getX(), result.getY() , result.getZ());
+        Triplet result = trip.schurProduct(c.getTrip());
+        return new Color(result.getX(), result.getY(), result.getZ());
     }
 }
--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Plane.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Plane.java
@@ -1,12 +1,24 @@
 package fr.univartois.butinfo.sae301;
 
-public class Plane extends SceneObject{
-	private int width;
-	private int height;
-	public Plane (Vector pos, Color color, int width, int height) {
-		super(pos, color);
-		this.width = width;
-		this.height = height;
-	}
+/**
+ * Represents a plane object in a 3D scene, which is a subclass of SceneObject.
+ * A Plane is defined by its position, color, width, and height.
+ */
+public class Plane extends SceneObject {
+    private int width;
+    private int height;
 
+    /**
+     * Constructs a Plane object with the specified position, color, width, and height.
+     *
+     * @param pos    The position vector of the plane.
+     * @param color  The color of the plane.
+     * @param width  The width of the plane.
+     * @param height The height of the plane.
+     */
+    public Plane(Vector pos, Color color, int width, int height) {
+        super(pos, color);
+        this.width = width;
+        this.height = height;
+    }
 }
