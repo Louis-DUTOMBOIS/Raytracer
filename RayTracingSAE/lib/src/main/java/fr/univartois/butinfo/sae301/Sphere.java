7a0e07b90aac8ba8a7c260961ddfbd1e37085fbc
Alex Pochet <thypux@gmail.com>
2023-10-24 11:02:18 +0200

    Ajout doc sphere

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
@@ -1,7 +1,21 @@
 package fr.univartois.butinfo.sae301;
 
+/*
+ * Represents a sphere object in a 3D scene.
+ */
 public class Sphere extends SceneObject{
+	/*
+     * The radius of the sphere.
+     */
 	private double radius;
+	
+	/*
+     * Creates a new sphere with the given vector, color and radius.
+     *
+     * @param vector The vector of the sphere.
+     * @param color The color of the sphere.
+     * @param radius The radius of the sphere.
+     */
 	public Sphere(Vector pos, Color color, double radius) {
 		super(pos, color);
 		this.radius = radius;
