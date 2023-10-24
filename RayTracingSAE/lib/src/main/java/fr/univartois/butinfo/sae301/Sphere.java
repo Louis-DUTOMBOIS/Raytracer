c4c7a34eb237acc42456dd6919228b6fce3f23f6
Alex Pochet <thypux@gmail.com>
2023-10-24 13:51:41 +0200

    Correction de certain objets de la scene (utilisation d'une interface maintenant)

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
@@ -3,11 +3,13 @@ package fr.univartois.butinfo.sae301;
 /*
  * Represents a sphere object in a 3D scene.
  */
-public class Sphere extends SceneObject{
+public class Sphere implements ISceneObject{
 	/*
      * The radius of the sphere.
      */
+	private Point pos;
 	private double radius;
+	private Color color;
 	
 	/*
      * Creates a new sphere with the given vector, color and radius.
@@ -16,9 +18,41 @@ public class Sphere extends SceneObject{
      * @param color The color of the sphere.
      * @param radius The radius of the sphere.
      */
-	public Sphere(Vector pos, Color color, double radius) {
-		super(pos, color);
+	public Sphere(Point pos, Color color, double radius) {
+		this.pos = pos;
+		this.radius = radius;
+		this.color = color;
+	}
+
+	@Override
+	public Point getOrigin() {
+		return pos;
+	}
+
+	public Point getPos() {
+		return pos;
+	}
+
+	public void setPos(Point pos) {
+		this.pos = pos;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
 		this.radius = radius;
 	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	
 
 }
