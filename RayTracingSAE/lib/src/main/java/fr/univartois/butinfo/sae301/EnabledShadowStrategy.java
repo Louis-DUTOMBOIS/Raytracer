8816581258a867e0523445517cb59a6c968bff09
Alex Pochet <thypux@gmail.com>
2023-10-25 17:22:52 +0200

    Ajout patron de conception strategy pour gestion des ombres

---
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/EnabledShadowStrategy.java
@@ -0,0 +1,11 @@
+package fr.univartois.butinfo.sae301;
+
+public class EnabledShadowStrategy implements IShadowStrategy{
+
+	@Override
+	public boolean detectShadow(Point intersectionPoint) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
