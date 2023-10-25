29257ff719bba89bfdec85fc807cf162c04325c8
Louis Dutombois <louis.dutombois@gmail.com>
2023-10-25 13:22:34 +0200

    Commit de la classe CreatePicture qui affiche enfin un rond

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/Sphere.java
@@ -63,10 +63,12 @@ public class Sphere implements ISceneObject{
 	public double intersect(Point p, Vector d) {
 	    Vector oc = p.subtraction(this.pos); // Vector from the center of the sphere to the ray's origin
 	 
-	    double a = d.scalarProduct(d);
-	    double b = 2.0 * oc.scalarProduct(d);
+	    double a = 1;
+	    double b = (oc.multiplicationScailary(2)).scalarProduct(d);
 	    double c = oc.scalarProduct(oc) - (this.radius*this.radius);
-	    double discriminant = (b * b) - (4.0 * a * c);
+	    double discriminant = (b * b) - 4.0 * a * c;
+	    
+	    System.out.println("a: "+ a+ " b : "+b+" c : "+c+" discri : "+discriminant+" oc: "+ oc);
 
 	    if (discriminant < 0) {
 	        return -1; // No intersection
