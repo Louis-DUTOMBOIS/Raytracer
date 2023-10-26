9ea9a035d702f2db00a238c2efc536411e210455
Theo Vambre <theo.vambre2003@gmail.com>
2023-10-26 12:52:56 +0200

    Debut des patron de conception et de la formule de Lambert #19

--- RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/LambertMethodStrategy.java
+++ RayTracingSAE/lib/src/main/java/fr/univartois/butinfo/sae301/LambertMethodStrategy.java
@@ -1,13 +1,19 @@
 package fr.univartois.butinfo.sae301;
 import java.awt.Color;
 
-public class LambertMethodStrategy implements IColorStrategy {
-	
-	
+public class LambertMethodStrategy implements IColorStrategy {	
 	@Override
 	public Color calculateColor(Vector normal, Light[] light, Point intersectionPoint, ISceneObject object) {
-		Color color = null;
-		return color;
+		Vector n = intersectionPoint.subtraction(cc);// centre de la sphere
+		Vector normN = n.normalize();
+		
+		//formule de lambert
+		double res1 = normN.scalarProduct(ldir);// ldir direction de la lumiere
+		double res2 = Math.max(res1, 0.0);
+		Color res3 = res2.schurProduct(lightColor); // couleur de la lumiere
+		Color x = res3.scalarProduct(cDiff); // couleur diffuse d'un objet
+		
+		// formule de la couleur finale
+		double col = null;
 	}
-
 }
